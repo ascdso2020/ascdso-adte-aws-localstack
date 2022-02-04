99b47d5e1b3a1273f5b6bf1a0afb9ed99e09c8cc
Stefanie Plieschnegger <stefanie@Stefanies-MacBook-Pro-2.local>
2022-02-04 17:00:03 +0100

    implemented update_state for alarm

--- localstack/services/cloudwatch/cloudwatch_starter.py
+++ localstack/services/cloudwatch/cloudwatch_starter.py
@@ -1,7 +1,10 @@
 import moto.cloudwatch.responses as cloudwatch_responses
+from moto.cloudwatch.models import FakeAlarm
 
 from localstack import config
 from localstack.services.infra import start_moto_server
+from localstack.utils.aws import aws_stack
+from localstack.utils.patch import patch
 
 
 def apply_patches():
@@ -20,6 +23,28 @@ def apply_patches():
 
     if not hasattr(cloudwatch_responses.CloudWatchResponse, "put_composite_alarm"):
         cloudwatch_responses.CloudWatchResponse.put_composite_alarm = put_composite_alarm
+
+    @patch(target=FakeAlarm.update_state)
+    def update_state(target, self, reason, reason_data, state_value):
+        target(self, reason, reason_data, state_value)
+
+        # check the state and trigger required actions
+        if self.actions_enabled:
+            actions = None
+            if self.state_value == "OK":
+                actions = self.ok_actions
+            elif self.state_value == "ALARM":
+                actions = self.alarm_actions
+            else:
+                actions = self.insufficient_data_actions
+            for action in actions:
+                data = aws_stack.parse_arn(action)
+                # test for sns - can this be done in a more generic way?
+                if data["service"] == "sns":
+                    service = aws_stack.connect_to_service(data["service"])
+                    # publish this way? what should go into subject/message etc?
+                    service.publish(TopicArn=action, Subject=self.name, Message=self.description)
+            # log warning -> not supported + add todo
 
 
 def start_cloudwatch(port=None, asynchronous=False, update_listener=None):
